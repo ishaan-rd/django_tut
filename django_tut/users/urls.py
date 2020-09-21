1bf3bd05a398a51f3d272a2eb4e92a183c2ed22f
ishaan-rd <ishaanrd6@gmail.com>
2020-09-21 13:25:17 +0530

    registeration done

--- django_tut/users/urls.py
+++ django_tut/users/urls.py
@@ -13,11 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('register/', include('users.urls')),
-    path('', include('blog.urls')),
+    path('', views.register, name='register'),
 ]
