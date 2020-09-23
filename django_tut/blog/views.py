19c1333e41446525727a579ddc2b6b5da57b6df0
ishaan-rd <ishaanrd6@gmail.com>
2020-09-23 12:09:00 +0530

    Class based views - ListView, CreateView, UpdateView, DeleteView - done

--- django_tut/blog/views.py
+++ django_tut/blog/views.py
@@ -1,12 +1,59 @@
 from django.shortcuts import render
+import django.views.generic as django_views
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
-def home(request):
-    context = {
-        'posts': Post.objects.all()
-    }
+# def home(request):
+#     context = {
+#         'posts': Post.objects.all()
+#     }
 
-    return render(request, 'blog/home.html', context)
+#     return render(request, 'blog/home.html', context)
+
+
+class PostListView(django_views.ListView):
+    model = Post
+    template_name = 'blog/home.html' # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(django_views.DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, django_views.CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, django_views.UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, django_views.DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
